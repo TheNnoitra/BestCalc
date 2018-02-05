c560044eeff7b93cce088eb7cb91e0d2f91e78fa
TheNnoitra <thennoitra@hotmail.com>
2018-02-05 15:22:49 +0400

    Добавлено 3 операции. Выбор операции и ввод чисел через консоль. Проверка на операции.

--- BestCalc/Program.cs
+++ BestCalc/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BestCalc
 {
     class Program
@@ -11,7 +12,65 @@ namespace BestCalc
         static void Main(string[] args)
         {
             Console.WriteLine("Калькулятор");
+
+            Console.Write("Выберите операцию (Sum, Min, Mul, Div): ");
+            string oper = Console.ReadLine();
+            bool Operaciya = Program.Proverka(oper);
+            if (Operaciya == true)
+            {
+                Console.Write("Введите числа (через пробел): ");
+                double x = 0;
+                double y = 0;
+                double res = 0;
+                string operXY = Console.ReadLine();
+                string[] array = operXY.Split(' ');//разделяет на массив чисел разделенных знаками
+                x = Convert.ToDouble(array[0]);
+                y = Convert.ToDouble(array[1]);
+                Console.WriteLine($"Введеные числа: x = {x}, y = {y}");
+
+                Calc calc = new Calc();
+                
+                if (oper == "Sum")
+                {
+                    res = calc.Sum(x,y);
+                    Console.WriteLine($"Sum({x} + {y}) = {Math.Round(res,4)}");
+
+                }
+                else if (oper == "Min")
+                {
+                    res = calc.Min(x, y);
+                    Console.WriteLine($"Min({x} - {y}) = {Math.Round(res, 4)}");
+
+                }
+                else if (oper == "Mul")
+                {
+                    res = calc.Mul(x,y);
+                    Console.WriteLine($"Mul({x} * {y}) = {Math.Round(res, 4)}");
+
+                }
+                else if (oper == "Div")
+                {
+                    res = calc.Div(x, y);
+                    Console.WriteLine($"Div({x} / {y}) = {Math.Round(res, 4)}");
+
+                }
+                
+            }
+            else { Console.WriteLine("Такой операции не обнаружено"); }
             Console.ReadKey();
+        }
+        public static bool Proverka(string oper)
+        {
+            bool otvet = false;
+            if (oper == "Sum" || oper == "Min" || oper == "Mul" || oper == "Div" || oper == "Rand")
+            {
+                otvet = true;
+            }
+            else
+            {
+                otvet = false;
+            }
+            return otvet;
         }
     }
 }
