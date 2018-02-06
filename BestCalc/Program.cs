27aaabb758a9bde5555781e4f4ab3547eb619e83
TheNnoitra <thennoitra@hotmail.com>
2018-02-06 15:07:44 +0400

    Добавлена проверка введеной операции в консоли (есть ли в наличии такая операция).

--- BestCalc/Program.cs
+++ BestCalc/Program.cs
@@ -6,33 +6,36 @@ using System.Threading.Tasks;
 using System.Reflection;
 
 
-
 namespace BestCalc
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             //получить сборку классов
             //является ли он реализацией нашего интерфейса
             var calc1 = new Calc();
             var operations = calc1.GetOperNames();
+
+
+
 
             Console.WriteLine("Калькулятор");
 
             Console.Write("Доступные операции:  ");
             foreach (var item in operations)
             {
-                Console.Write(item+" ");
+                //вывод в консоль список доступных операций
+                Console.Write(item+" ");    
             }
             Console.WriteLine();
             Console.Write("Выберите операцию:  ");
 
             string oper = Console.ReadLine();
-            bool Operaciya = Program.Proverka(oper);
 
-            if (Operaciya == true)
+            //проверка введеной операции (есть ли такая в наличии)
+            bool opers = operations.Any(el => el.Contains(oper)); 
+            if (opers == true)
             {
                 double x = 0;
                 double y = 0;
@@ -40,7 +43,8 @@ namespace BestCalc
 
                 Console.Write("Введите числа (через пробел): ");
                 string operXY = Console.ReadLine();
-                string[] array = operXY.Split(' ');//разделяет на массив чисел разделенных знаками
+                //разделяет на массив чисел разделенных знаками
+                string[] array = operXY.Split(' ');
                 x = Convert.ToDouble(array[0]);
                 y = Convert.ToDouble(array[1]);
                 Console.WriteLine($"Введеные числа: x = {x}, y = {y}");
@@ -56,7 +60,7 @@ namespace BestCalc
         public static bool Proverka(string oper)
         {
             bool otvet = false;
-            if (oper == "Sum" || oper == "Min" || oper == "Mul" || oper == "Div" || oper == "Rand")
+            if (oper == "Sum" || oper == "Min" || oper == "Mul" || oper == "Div")
             {
                 otvet = true;
             }
